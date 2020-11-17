613d4bcd83962f2bb50a0c4fa2b2892d0b1361d3
jingo <williamjingo@yahoo.co.uk>
2020-11-17 19:25:07 +0300

    added portfolio imgs

--- src/services/portfolioService.js
+++ src/services/portfolioService.js
@@ -3,7 +3,7 @@ const portfolio = [
     id: 1,
     title: "Giant Aluminum Application",
     img:
-      "https://georgejingo.com/wp-content/uploads/apollo13_images/giant-6zfa26vsuknh3f52yk3pi7mv9x2o64ut0a.jpg",
+      "https://res.cloudinary.com/wljin/image/upload/v1605629837/personal%20website/giant_lfqidh.png",
     stack: ["laravel", "vuejs"],
     description:
       "I am pleased to have contributed as the project manager, and lead developer in defining the way Giant Aluminium handles information and how it generally makes business processes easier, more efficient and streamlined in satisfying particular information needs. With software that handles everything right from manufacturing to points of sale, Ofcourse I leant so much 🙂 , achieved a great deal and found this a very rewarding experience in business and technical perspective",
@@ -12,7 +12,7 @@ const portfolio = [
     id: 2,
     title: "Dispatch Admin Dashboard",
     img:
-      "https://georgejingo.com/wp-content/uploads/apollo13_images/dispatch-6zfa7sohpvrud57qzhg0qslo6hdwt2fql6.jpg",
+      "https://res.cloudinary.com/wljin/image/upload/v1605629385/personal%20website/dispatch_wqk6dp.png",
     stack: ["react"],
     description:
       "As a frontend developer, I was contracted to build a web dashboard to visualize dispatch data for an ambulance dispatch service in uganda.",
@@ -21,7 +21,7 @@ const portfolio = [
     id: 3,
     title: "Cplan",
     img:
-      "https://willappsug.com/wp-content/uploads/apollo13_images/Regent-Estates-4-6yn27w1jbylq18gpwef3rchpbsy028atp2.png",
+      "https://res.cloudinary.com/wljin/image/upload/v1605629866/personal%20website/cplan_mh1kfq.png",
     stack: ["laravel", "react"],
     description:
       "As the team lead and backend developer in the development of the CPlan Platform. I actively participated in activities such as communication with the user community during requirements analysis, prototyping, system analysis, development, fielding, and maintenance of systems",
@@ -30,7 +30,7 @@ const portfolio = [
     id: 4,
     title: "AWALI",
     img:
-      "https://willappsug.com/wp-content/uploads/apollo13_images/Siha-a-6yg5zhsv1ie5ab7fneuchxsz8ty671968m.png",
+      "https://res.cloudinary.com/wljin/image/upload/v1605629316/personal%20website/awali_nv7pk0.png",
     stack: ["laravel", "react"],
     description:
       "As a full stack developer and freelancer, I built the Awali movie streaming service, currently in its last tests before going to production in Jan 2021.",
@@ -39,7 +39,7 @@ const portfolio = [
     id: 5,
     title: "NGB",
     img:
-      "https://georgejingo.com/wp-content/uploads/apollo13_images/ngb-6zfa9dfdz2cr1qgee68qstv16id8nemh5m.jpg",
+      "https://res.cloudinary.com/wljin/image/upload/v1605629414/personal%20website/ngb_kehv4k.png",
     stack: ["react", "nodejs"],
     description:
       "As a fullstack developer, I worked on the NGB Application an application that handles Operator Management, Human resource management, License management, betting machines management and premises management.",
@@ -48,7 +48,7 @@ const portfolio = [
     id: 6,
     title: "Wheatco Business Admin",
     img:
-      "https://georgejingo.com/wp-content/uploads/apollo13_images/wheatco-6zfab4je6l41hn5tzw50xs8gzh3qv8nwwq.jpg",
+      "https://res.cloudinary.com/wljin/image/upload/v1605629414/personal%20website/ngb_kehv4k.png",
     stack: ["laravel", "react"],
     description:
       "As a fullstack developer at willapps limited, I participated in the building of a business management application for wheatco industries.",
