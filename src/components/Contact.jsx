fc92133beb6cef0851937cae474139c1fbfb8b16
Jingo <49089047+williamjingo@users.noreply.github.com>
2021-06-27 14:59:26 +0300

    Added rel to href links with _blank target

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,9 +3,9 @@ import React from 'react'
      return ( 
          <>
          <h2 className="mb-3">Reachout</h2>
-         <p>If you want to reachout to me or just wana say 👋 "Hi", please send me a messege via <a href="https://twitter.com/williamjingo" target="_blank">twitter</a> or <a href="https://www.linkedin.com/in/william-jingo/" target="_blank">linkedin</a>.</p>
+         <p>If you want to reachout to me or just wana say 👋 "Hi", please send me a messege via <a href="https://twitter.com/williamjingo" target="_blank" rel="noreferrer">twitter</a> or <a href="https://www.linkedin.com/in/william-jingo/" target="_blank" rel="noreferrer">linkedin</a>.</p>
          <hr/>
-         <p>If you like this concept and want to reuse the code as your own, clone or fork the repository from <a href="https://github.com/williamjingo/jingo" target="_blank">github</a>. Don't forget to forward me a link to your portfolio via <a href="https://twitter.com/williamjingo" target="_blank">twitter</a>. Cheers! </p>
+         <p>If you like this concept and want to reuse the code as your own, clone or fork the repository from <a href="https://github.com/williamjingo/jingo" target="_blank" rel="noreferrer">github</a>. Don't forget to forward me a link to your portfolio via <a href="https://twitter.com/williamjingo" target="_blank" rel="noreferrer">twitter</a>. Cheers! </p>
          </>
       );
  }
